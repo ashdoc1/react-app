1221e77d4c283a12b075fe0c439883b54261d38a
ashdoc1 <ashishgoelhr@gmail.com>
2024-05-23 21:37:11 +0100

    .

--- frontend/pizza-menu/src/App.js
+++ frontend/pizza-menu/src/App.js
@@ -10,7 +10,7 @@ const App = () => {
                 <Joke />
             </div>
             <div>
-                <PizzaList />
+                <PizzaMenu />
             </div>
         </div>
     )
--- frontend/pizza-menu/src/pizza_list.js
+++ frontend/pizza-menu/src/pizza_list.js
@@ -44,4 +44,4 @@ const PizzaList = () => {
   );
 };
 
-export default PizzaList;
+// export default PizzaList;
